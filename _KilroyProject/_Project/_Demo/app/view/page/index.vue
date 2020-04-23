4bbc7f4cca386783b29c57f6b92beec61c412706
kilroy_lyk <736587340@qq.com>
2020-04-23 19:31:04 +0800

    KILROY

---
+++ _KilroyProject/_Project/_Demo/app/view/page/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <section id="index" class="index">
+        KILROY
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'Index',
+        props: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
