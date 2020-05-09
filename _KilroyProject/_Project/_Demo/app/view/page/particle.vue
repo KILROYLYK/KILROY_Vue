1c14869317220a267ef47e3fb5796133d8069720
kilroy_lyk <736587340@qq.com>
2020-05-09 14:09:39 +0800

    KILROY

---
+++ _KilroyProject/_Project/_Demo/app/view/page/particle.vue
@@ -0,0 +1,21 @@
+<template>
+    <canvas id="particle" class="particle" />
+</template>
+
+<script lang="ts" type="text/tsx">
+    export default {
+        name: "Particle",
+        props: {},
+        data: (component) => {
+            const _this = this,
+                props = {};
+            
+            return {}
+        }
+    }
+</script>
+
+<style lang="less" rel="stylesheet/less" scoped>
+    @import '../../../../_Base/css/base.less';
+
+</style>
